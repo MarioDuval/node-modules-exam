9ffbc4a5ee23a7bf0ab70276512ad349fee3b612
MarioDuval <77450981+MarioDuval@users.noreply.github.com>
2021-12-10 19:09:14 +0100

    main

--- managers/manager-fs.js
+++ managers/manager-fs.js
@@ -3,10 +3,24 @@ import fs from 'fs';
 // afegiu codi ... (2pto)
 export class ManagerFs {
     constructor(_file) {
-        //...
+        // this.mgfl = new ManagerFs('./data/movies.json');
+        // this.mgfl2 = new ManagerFs('./data/actors.json');
+        // this.actors = this.mgfl.getData();
+        // this.movies = this.mgfl2.getData();
+        this.file = _file('./data/movies.json');
+        this.movies = this.file.getData();
+
+        return this.movies;
+        
     }
     getData() {
-        //...
+
+        try {
+            const data = fs.readFileSync(this.file, "utf8");
+            console.log("File content:", data);
+        } catch (err) {
+            console.error(err);
+        }
 
     }
 }
